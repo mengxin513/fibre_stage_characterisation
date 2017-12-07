54d9e127610bfe8d20e4bb6572d0ceada9c0ac9b
Ed <qm237@bath.ac.uk>
2017-12-07 17:11:08 +0000

    hill walk changes

--- hill_walk.py
+++ hill_walk.py
@@ -1,20 +1,21 @@
 
 from openflexure_stage import OpenFlexureStage
 import numpy as np
-import h5py
-import time
 import math
+import time
+import h5py
 import data_file
 
-def take_data(stage, points, samples, direction, step_size, axis):
+def take_data(stage, points, samples, step_size, axis):
     data = np.zeros((points, 5))
-    avg = np.zeros((samples, 1))
-    stage.move_rel(np.dot(math.trunc(-(step_size * points / 2)), axis))
+    averages = np.zeros((samples, 1))
+    stage.move_rel(np.dot(-(step_size * math.trunc(points / 2)), axis))
     for i in range(points):
+        stage.move_rel(np.dot(step_size, axis))
         for j in range(samples):
-            avg[j, 0] = stage.light_sensor_fullspectrum
+            averages[j, 0] = stage.light_sensor_fullspectrum
             time.sleep(0.1)
-        data[i, 0] = np.mean(avg, axis = 0)
+        data[i, 0] = np.mean(averages, axis = 0)
         data[i, 1] = stage.light_sensor_gain
         data[i, 2:] = stage.position
     return data
@@ -26,53 +27,80 @@ if __name__ == "__main__":
         stage.light_sensor_gain = 9876
         step_size = 500
         min_step = 50
-        points = 10
-        samples = 10
-        m = 1
+        points = 5
+        samples = 5
+        j = 1
 
-        backlash = 256
+        stage.backlash = 256
 
         df = data_file.Datafile(filename = "hillwalk.hdf5")
-        data_intensity = df.new_group("intensity reading", "hill walk")
+        data_intensity = df.new_group("intensity reading over time", "hill walk")
+        raw_data = df.new_group("data taken by take_data function", "hill walk")
 
-        intensity_data = np.zeros((500, 5))
+        intensity_data = np.zeros((100, 5))
 
         start_time = time.time()
-        
+
         intensity_data[0, 0] = start_time
         intensity_data[0, 1] = stage.light_sensor_fullspectrum
         intensity_data[0, 2:] = stage.position
+        df.add_data(intensity_data, data_intensity, "intensity reading")
 
         while step_size > min_step:
-            for axis in [[1, 0, 0], [0, 1, 0], [0, 0, 1]]:
-                    data = take_data(stage, points, samples, direction, step_size, axis)
+            for axis in [[1, 0, 0], [0, 1, 0], [0, 0, 10]]:
+                while True:
+                    data = take_data(stage, points, samples, step_size, axis)
                     print data
-                    for i in range(data.shape[0]):
-                        if data[i, 0] < 10:
-                            current_gain = stage.light_sensor_gain
-                            if current_gain < np.max(gain):
-                                stage.light_sensor_gain = np.min([g for g in gain if g > current_gain])
-                                continue
-                        elif data[i, 0] > 2**15:
-                            current_gain = stage.light_sensor_gain
-                            if current_gain > np.min(gain):
-                                stage.light_sensor_gain = np.max([g for g in gain if g < current_gain])
-                                continue
+                    df.add_data(data, raw_data, "data taken by take_data function")
+                    if np.all(data[:, 0] < 10) == True:
+                        current_gain = stage.light_sensor_gain
+                        if current_gain < np.max(gain):
+                            stage.light_sensor_gain = np.min([g for g in gain if g > current_gain])
+                            continue
+                    elif np.all(data[:, 0] > 2**15) == True:
+                        current_gain = stage.light_sensor_gain
+                        if current_gain > np.min(gain):
+                            stage.light_sensor_gain = np.max([g for g in gain if g < current_gain])
+                            continue
+                    coefficients = np.polyfit(np.dot(data[:, 2:], axis), data[:, 0], 2)
+                    if 2 * coefficients[0] > 0:
+                        coefficient = np.polyfit(np.dot(data[:, 2:], axis), data[:, 0], 1)
+                        if coefficient[0] > 0:
+                            print "straight line with positive gradient"
+                            stage.move_abs(data[points-1, 2:])
+                            continue
+                        elif coefficient[0] < 0:
+                            print "straight line with negative gradient"
+                            stage.move_abs(data[0, 2:])
+                            continue
+                    elif 2 * coefficients[0] < 0:
+                        gradients = np.zeros((points, 1))
+                        gradients[:, 0] = (2 * coefficients[0] * np.dot(data[:, 2:], axis)) + coefficients[1]
+                        signs_sum = np.sum(np.sign(gradients[:, 0]))
+                        if signs_sum == points:
+                            print "Not at the peak yet, working hard on it"
+                            stage.move_abs(data[points-1, 2:])
+                            continue
+                        elif signs_sum == -points:
+                            print "Getting bored? I'm nearly there"
+                            stage.move_abs(data[0, 2:])
+                            continue
                         else:
-                            pass
-                    coe = np.polyfit(np.dot(data[:, 2:], axis), data[:, 0], 2)
-                    if coe[0] = 0:
-                        if coe[1] > 0
-                        stage.move_abs(data[points-1, 2:])
-                        break
-                        elif coe[1] < 0
-                        stage.move_abs(data[0, 2:])
-                        break
-                    elif coe[0] != 0:
-                        pos_to_be = -(coe[1] / 2 * coe[0])
-                        stage.move_rel(np.dot((pos_to_be - np.dot(data[points-1, 2:], axis)), axis))
-                        break
+                            print "Look I just found the peak"
+                            pos_to_be = -(coefficients[1] / (2 * coefficients[0]))
+                            stage.move_rel(np.dot((pos_to_be - np.dot(data[points-1, 2:], axis)), axis)/np.sum(np.array(axis)**2))
+                            intensity_data[j, 0] = time.time() - start_time
+                            intensity_data[j, 1] = stage.light_sensor_fullspectrum
+                            intensity_data[j, 2:] = stage.position
+                            j = j + 1
+                            break
                 
             step_size = np.rint(step_size / 2)
-
-        df.add_data(data, data_intensity, "intensity reading")
+            print "Step size reduced to %d" %step_size
+            
+        df.add_data(intensity_data, data_intensity, "intensity reading over time")
+            
+        print stage.light_sensor_fullspectrum
+        print stage.light_sensor_gain
+        print stage.position
+        
