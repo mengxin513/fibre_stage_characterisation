db39f977e12ecd4a418524952902c9098fe8a841
mengxin513 <33423666+mengxin513@users.noreply.github.com>
2018-07-04 10:55:53 +0100

    Add files via upload

--- drift_twitter.py
+++ drift_twitter.py
@@ -1,70 +1,63 @@
 
-#import libraries
 import cv2
 import numpy as np
 from scipy import ndimage, signal
-import io
-import sys
 import time
 from camera_stuff import get_numpy_image, find_template
 import microscope
 import picamera
 import data_file
 from openflexure_stage import OpenFlexureStage
-import threading
-import matplotlib.pyplot as plt
-import matplotlib
 import data_file
 import h5py
 import tweepy
 import twitter_keys
+from contextlib import closing
 
 if __name__ == "__main__":
 
-    with picamera.PiCamera(resolution=(640,480)) as camera, OpenFlexureStage('/dev/ttyUSB0') as stage:
-        
+    with picamera.PiCamera(resolution = (640, 480)) as camera, \
+         OpenFlexureStage('/dev/ttyUSB0') as stage, \
+         closing(data_file.Datafile(filename = "drift.hdf5")) as df:
+
         #authorises the program to acces the twitter account
         auth = tweepy.OAuthHandler(twitter_keys.consumer_key, twitter_keys.consumer_secret)
-        auth.set_access_token(twitter_keys.access_token,twitter_keys.access_token_secret)
+        auth.set_access_token(twitter_keys.access_token, twitter_keys.access_token_secret)
         api = tweepy.API(auth)
         
         N_frames = 100
-        counter = 0
 
-        #loads camera from picamera and set the stage as the arduino, lower resolution can speed up the programme
         ms = microscope.Microscope(camera, stage)
         ms.freeze_camera_settings()
         frame = get_numpy_image(camera, True)
 
-        df = data_file.Datafile(filename="drift_81217.hdf5") #creates the .hdf5 file to save the data
-        data_gr = df.new_group("test_data", "A file of data collected to test this code works")
-        #puts the data file into a group with description
-
-        image = get_numpy_image(camera, greyscale=True) #takes template photo
-        templ8 = image[100:-100,100:-100] #crops image
+        df = data_file.Datafile(filename = "drift.hdf5")
+        cam_pos = df.new_group("data", "drift")
 
         loop = True
         tweet = True
 
-        start_t = time.time() #measure starting time
+        image = get_numpy_image(camera, greyscale = True).astype(np.float32)
+        background = cv2.GaussianBlur(image, (41, 41), 0)
+        templ8 = (image - background)[100:-100, 100:-100]
 
+        start_t = time.time()
 
         try:
             while loop:
-                data = np.zeros((3, N_frames)) #creates the array in which data is stored
-                for i in range(data.shape[1]):
-                #takes 100 images and compares the location of the spot with the template
+                data = np.zeros((N_frames, 3))
+                for i in range(N_frames):
                     frame = get_numpy_image(camera, True)
-                    spot_coord = find_template(templ8, frame)
-                    data[1, i] = spot_coord[0]
-                    data[2, i] = spot_coord[1]
-                    data[0, i] = time.time() - start_t
-                df.add_data(data, data_gr, "data") #writes data to .hdf5 file
+                    data[i, 1:], corr = find_template(templ8, frame - background, return_corr = True)
+                    cv2.imshow("corr", corr.astype(float) / np.max(corr) * 255)
+                    cv2.waitKey(1000)
+                    data[i, 0] = time.time() - start_t
+                df.add_data(data, cam_pos, "data")
                 imgfile_location = "/home/pi/dev/fibre_stage_characterisation/frames/drift_%s.jpg" % time.strftime("%Y%m%d_%H%M%S")
                 cv2.imwrite(imgfile_location, frame)
                 try:
                     if time.gmtime(time.time())[3] in [0, 4, 8, 12, 16, 20] and tweet: #tweets a picture and co-ordinates every 4 hours
-                        api.update_with_media(imgfile_location, status="I'm currently at %d, %d" %(spot_coord[0], spot_coord[1]))
+                        api.update_with_media(imgfile_location, status = "I'm currently at %d, %d" %(spot_coord[0], spot_coord[1]))
                         tweet = False
                     elif time.gmtime(time.time())[3] not in [0, 4, 8, 12, 16, 20]:
                         tweet = True
